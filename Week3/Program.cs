02b077914b41c4ba7d774895971c43e0276de9de
Opilane TTHK <opilane@tthk.ee>
2020-09-14 14:28:03 +0300

    week3

---
+++ Week3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Week3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login, password;
+            Console.WriteLine("login: ");
+            login = Console.ReadLine();
+            Console.WriteLine("password: ");
+            password = Console.ReadLine();
+            if (login == "admin" && password == "pass1234")
+            {
+                Console.WriteLine("welcome,admin!");
+            }
+            else
+            {
+                Console.WriteLine("login failed.try again.");
+            }
+            
+
+        }
+    }
+}
